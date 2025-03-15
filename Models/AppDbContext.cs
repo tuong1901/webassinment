68f787232ffedfe741984f99905ca219ea7f6125
tuong1901 <hoanghoangcattuong@gmail.com>
2025-03-16 01:00:56 +0700

    Add files via upload

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,36 @@
+using App.Models.Contacts;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Models 
+{
+    // App.Models.AppDbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
